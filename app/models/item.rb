5e9100864a5e0c96539c3882a31598a4978a9d90
John Nkp <john.nkp1@gmail.com>
2021-01-28 08:33:45 +1100

    implemented simple forms

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,7 @@
 class Item < ApplicationRecord
+
+    BRAND = %w{ Other Nike Addidas TommyHilfiger Puma Zara }
+    CONDITION = %w{ Other New Used}
+    SIZE = %w{Other Small Meduim Large Xlarge}
+    COLOR = %w{Other Black White Green Red Blue Pink Orange Yellow Purple}
 end
